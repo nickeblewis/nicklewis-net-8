da0b8b8603a68bf2c8e353429c5a4423ff28568d
nicklewiskier <nick.lewis2@kier.co.uk>
2019-12-11 16:23:28 +0000

    categories restored

--- studio/schemas/documents/post.js
+++ studio/schemas/documents/post.js
@@ -63,6 +63,19 @@ export default {
       ]
     },
     {
+      name: 'categories',
+      type: 'array',
+      title: 'Categories',
+      of: [
+        {
+          type: 'reference',
+          to: {
+            type: 'category'
+          }
+        }
+      ]
+    },
+    {
       title: 'Tags',
       name: 'tags',
       type: 'array',
