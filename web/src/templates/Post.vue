8b43767ea2d9398e1e7df25e603870053e11d8c5
nicklewiskier <nick.lewis2@kier.co.uk>
2019-12-18 16:35:50 +0000

    hiddent ags

--- web/src/templates/Post.vue
+++ web/src/templates/Post.vue
@@ -3,7 +3,8 @@
     <div class="container-inner mx-auto my-16">
       <h1 class="text-4xl font-bold leading-tight">{{ $page.post.title }}</h1>
       <div class="text-xl text-gray-600 mb-4">{{ $page.post.date }}</div>
-      <div class="flex mb-8 text-sm">
+      <!-- TODO - come up with a better UI for these tags and reintroduce them -->
+      <!-- <div class="flex mb-8 text-sm">
         <g-link
           :to="tag.path"
           v-for="tag in $page.post.tags"
@@ -11,7 +12,7 @@
           class="bg-gray-300 rounded-full px-4 py-2 mr-4 hover:bg-green-300">
           {{ tag.title }}
         </g-link>
-      </div>
+      </div> -->
       <div class="markdown-body mb-8" v-html="$page.post.content" />
       <div class="mb-8">
         <g-link to="/blog" class="font-bold uppercase">Back to Blog</g-link>
