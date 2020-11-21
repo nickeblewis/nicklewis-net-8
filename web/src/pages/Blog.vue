a55b7ea666f8c849f9ee003623ec3e5b80a4a23f
Nick Lewis <nickeblewis@gmail.com>
2020-11-21 22:33:38 +0000

    updates to graphql

--- web/src/pages/Blog.vue
+++ web/src/pages/Blog.vue
@@ -23,6 +23,104 @@
   </div>
 </template>
 
+<static-query>
+query {
+  metadata {
+    sanityOptions{
+      projectId
+      dataset
+    }
+  }
+  settings: sanitySiteSettings(id: "siteSettings") {
+    title
+    description
+    homeBio
+    author {
+      name
+      image {
+        asset {
+          _id
+          url
+        }
+        hotspot {
+          x
+          y
+          width
+          height
+        }
+        crop {
+          top
+          left
+          right
+          bottom
+        }
+      }
+    }
+  }
+}
+</static-query>
+
+<page-query>
+query HomePosts {
+  metadata {
+    sanityOptions {
+      projectId
+      dataset
+    }
+  }
+  photos: allSanityPost (sortBy: "publishedAt", order: DESC, limit: 12) {
+    totalCount
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        id
+        title
+        tags
+        publishedAt (format: "MMMM D, Y")
+        authors {
+          author {
+            name
+            image {
+              asset {
+                url
+              }
+            }
+          }
+        }
+        _rawExcerpt
+        _rawBody
+        mainImage {
+          asset {
+            _id
+            url
+          }
+          caption
+          alt
+          hotspot {
+            x
+            y
+            height
+            width
+          }
+          crop {
+            top
+            bottom
+            left
+            right
+          }
+        }
+        slug {
+          current
+        }
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
 import Header from './../partials/Header.vue'
 import PageIllustration from '../partials/PageIllustration.vue'
